155e4276f29a70cf6ff4d40b8bb1d0ccca7bf773
aytsunnn <152917686+aytsunnn@users.noreply.github.com>
2025-04-05 17:51:46 +0400

    фильтры

--- src/app/components/AgroFilters.tsx
+++ src/app/components/AgroFilters.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { FilterType } from './types';
+import { FilterType } from "./types";
 
 interface AgroFiltersProps {
   filter: FilterType;
@@ -11,18 +9,38 @@ export default function AgroFilters({ filter, setFilter }: AgroFiltersProps) {
   return (
     <div className="fixed bottom-0 left-0 p-4 bg-white shadow-md w-full">
       <div className="mb-4 flex items-center gap-4">
-        <span className="text-sm font-medium text-gray-700">Фильтровать по:</span>
+        <span className="text-lg text-[#4E3000] font-bold">
+          Фильтровать по:
+        </span>
         <button
-          onClick={() => setFilter('absent')}
-          className={`px-3 py-1 text-sm rounded ${filter === 'absent' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          onClick={() => setFilter("absent")}
+          className={`px-3 py-1 text-sm rounded ${
+            filter === "absent"
+              ? "bg-[#C4C4C4] rounded-lg text-white"
+              : "text-[#4E3000]"
+          }`}
         >
           Отсутствующим
         </button>
         <button
-          onClick={() => setFilter('present')}
-          className={`px-3 py-1 text-sm rounded ${filter === 'present' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}
+          onClick={() => setFilter("present")}
+          className={`px-3 py-1 text-sm rounded ${
+            filter === "present"
+              ? "bg-[#C4C4C4] rounded-lg text-white"
+              : "text-[#4E3000]"
+          }`}
         >
           Присутствующим
+        </button>
+        <button
+          onClick={() => setFilter("all")}
+          className={`px-3 py-1 text-sm rounded ${
+            filter === "all"
+              ? "bg-[#C4C4C4] rounded-lg text-white"
+              : "text-[#4E3000]"
+          }`}
+        >
+          Без фильтра
         </button>
       </div>
     </div>
